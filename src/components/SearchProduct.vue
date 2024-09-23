6b283727565a7f31537b9248eddd347148e067bc
shpakvlad <shpakov.vladyslav@gmail.com>
2024-09-23 21:38:18 +0300

    added search product (work NOT right)

--- src/components/SearchProduct.vue
+++ src/components/SearchProduct.vue
@@ -1,14 +1,42 @@
 <script setup>
 
 import axios from "axios";
+import {ref} from "vue";
+import {useRoute} from "vue-router";
+import Product from "@/components/Product.vue";
+
+const searchList = ref([]);
+const route = useRoute();
 
 
+axios.get(`https://dummyjson.com/products/search?q=${route.params.searchText}`)
+    .then((response) => {
+      searchList.value = response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    }
+);
 </script>
 
 <template>
 
 <!--  todo реальзоваить страницу поиска товара-->
-      <h1>Hello wrold {{ $route.params.searchText}}</h1>
+  <div class="item-container">
+
+    <div  class="category-list-content">
+
+      <div v-if="searchList.products.length === 0">
+        <h1>Products not found!</h1>
+      </div>
+
+      <div v-else v-for="item of searchList.products" class="category-list" >
+
+        <Product :item="item" :key="searchList.products.id"></Product>
+
+      </div>
+    </div>
+  </div>
 
 
 
@@ -16,6 +44,76 @@ import axios from "axios";
 
 </template>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
+.item-container {
+  .category-name {
+    margin-left: 50px;
+    text-transform: uppercase;
+    position: absolute;
+  }
 
+  .category-list-content {
+    width: 100%;
+    padding: 40px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+
+    .category-list-item {
+      padding: 15px;
+      cursor: pointer;
+      max-width: 280px;
+      background-color: var(--color-background-mute);
+      border: 1px solid var(--color-border);
+      transition: scale .3s;
+
+
+      .img-container {
+        width: 250px;
+        height: 250px;
+
+        img {
+          width: 100%;
+          height: 100%;
+
+          background-color: var(--color-img-bg);
+          border-radius: 5px;
+        }
+      }
+
+      .category-list-content {
+        padding: 0 10px;
+        display: flex;
+        flex-flow: column;
+        height: 280px;
+        overflow: auto;
+
+        .item-title {
+          margin-top: 15px;
+          font-size: 1.2em;
+          overflow-wrap: break-word;
+        }
+
+        .item-price {
+          margin-top: 20px;
+          text-align: right;
+        }
+
+        .item-stock {
+          margin-top: 10px;
+        }
+
+        .add-to-cart {
+          padding: 10px;
+          text-transform: uppercase;
+          margin-top: auto;
+        }
+      }
+      &:hover {
+        scale: 1.03;
+        border-radius: 5px;
+      }
+    }
+  }
+}
 </style>
